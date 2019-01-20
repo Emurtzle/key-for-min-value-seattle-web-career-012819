abe510647ad2262d7e5bbff7c581fe05a28360ce
Emily Wagner <emilywagner598@gmail.com>
2019-01-20 00:56:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,10 @@
 def key_for_min_value(name_hash)
   min = 99999
   counter = 0;
-  index = 0;
+  :index
   name_hash.each do |name, int|
     if int < min
-      index = counter
+      index = name
     end
     counter += 1
   end
