d6ac5b8d375b4c59b8841150fffe7e5778f78078
Emily Wagner <emilywagner598@gmail.com>
2019-01-20 00:40:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min = 99999
+  name_hash.each do |name, age|
+    age < min ? min = age
+  end
+  min
 end
