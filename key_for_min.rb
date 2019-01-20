03b0c20b86f581b5575c7534105e3aa902dbce0d
Emily Wagner <emilywagner598@gmail.com>
2019-01-20 01:06:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   minimum = 99999
   min_name = ""
   name_hash.each do |name, int|
-    if int < min
+    if int < minimum
       minimum = int
       min_name = name
     end
