f0677e42a52e9290a334b91938ad36bf8b38ecb0
Emily Wagner <emilywagner598@gmail.com>
2019-01-20 01:08:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,13 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  minimum = 99999
-  min_name = ""
-  name_hash.each do |name, int|
-    if int < minimum
-      minimum = int
-      min_name = name
+  if block_given?
+    minimum = 99999
+    min_name = ""
+    name_hash.each do |name, int|
+      if int < minimum
+        minimum = int
+        min_name = name
+      end
     end
+    min_name
   end
-  min_name
 end
